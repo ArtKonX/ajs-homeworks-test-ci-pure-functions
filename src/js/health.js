657d636b223e49fc85d465d82498a1dcdf3f35ab
Artemij Kondurov <kondurovartemij@yandex.ru>
2024-06-14 17:43:27 +0300

    The data has been corrected

--- src/js/health.js
+++ src/js/health.js
@@ -4,9 +4,9 @@ export default function getHealth(objHealth) {
 
     if (objHealth.health < 0) throw new Error('The object value is a negative number');
     if (typeof (objHealth.health) !== 'number') throw new Error('The value of the object is not a number')
-    if (objHealth.health > 50) healthStatus = 'зелёный';
-    if (objHealth.health < 50 && objHealth.health > 15) healthStatus = 'жёлтый';
-    if (objHealth.health < 15) healthStatus = 'красный';
+    if (objHealth.health > 50) healthStatus = 'healthy';
+    if (objHealth.health < 50 && objHealth.health > 15) healthStatus = 'wounded';
+    if (objHealth.health < 15) healthStatus = 'critical';
 
     return healthStatus;
 
